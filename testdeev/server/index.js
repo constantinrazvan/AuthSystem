3b9c7bad86f32c4ebf9a199544768c0a61981d51
Razvan <76180811+constantinrazvan@users.noreply.github.com>
2022-11-04 22:59:41 +0200

    main

--- testdeev/server/index.js
+++ testdeev/server/index.js
@@ -1,17 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
 
 const app = express();
+const port = 9000;
 
-mongoose
-    .connect("mongodb://127.0.0.1:27017", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("connected to db");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, function() {
+    console.log(`Server is running on ${port} port!`);
+})
+
 
---
+++ testdeev/server/routes/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const router = express.Router;
+
+router.post('registerPage');
+router.post('loginPage');
+router.post('homePage');
